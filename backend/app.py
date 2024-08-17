6deaa7daf1011dc6f3c4eb9deaad40c44bcb70f2
mohamed <mohamed.maarka@outlook.fr>
2024-08-18 01:35:31 +0200

    front av deploiment

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from models import db
 from routes.auth import auth_bp
@@ -8,13 +8,12 @@ from dotenv import load_dotenv
 
 def create_app():
     load_dotenv()  # Charger les variables d'environnement depuis .env
-    
+
     app = Flask(__name__)
 
     # Configuration de la base de données
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///local.db')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['CORS_HEADERS'] = 'Content-Type'
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'your_secret_key_here')  # Clé secrète pour JWT
 
     # Initialiser SQLAlchemy avec l'application
@@ -22,10 +21,6 @@ def create_app():
 
     # Configurer les CORS
     CORS(app, resources={r"/api/*": {"origins": "*"}})
-
-    # Enregistrer les blueprints
-    app.register_blueprint(auth_bp, url_prefix='/api/auth')
-    app.register_blueprint(offers_bp, url_prefix='/api/offers')
 
     # Ajouter une route Hello World
     @app.route('/hello', methods=['GET'])
@@ -36,6 +31,19 @@ def create_app():
     @app.route('/test', methods=['GET'])
     def test_route():
         return jsonify(message="Test route is working")
+
+    # Ajouter un traitement pour les requêtes OPTIONS
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        if request.method == 'OPTIONS':
+            response.status_code = 200
+        return response
+
+    # Enregistrer les blueprints
+    app.register_blueprint(auth_bp, url_prefix='/api/auth')
+    app.register_blueprint(offers_bp, url_prefix='/api/offers')
 
     return app
 
